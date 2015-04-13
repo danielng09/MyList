62aec348347e4e9cf77efd552b42f5f102a24097
Daniel Ng <dvng13@gmail.com>
2015-04-13 01:13:05 -0600

    Finished basic search action

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,10 @@
 class Post < ActiveRecord::Base
+	before_save :set_keywords
+
+	scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
+
+	protected
+		def set_keywords
+			self.keywords = [title, price.to_s, body].map(&:downcase).join(" ")
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413034408) do
+ActiveRecord::Schema.define(version: 20150413070224) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150413034408) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "amazon_id"
+    t.text     "keywords"
   end
 
 end
