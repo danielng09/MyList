31d70b24681326d4a00560b0dc5dc3587036dd63
Daniel Ng <dvng13@gmail.com>
2015-04-12 20:01:13 -0600

    Generated post scaffold and initial seed

---
+++ app/views/posts/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @post, :id, :title, :price, :body, :created_at, :updated_at
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.destroy_all
+
+Post.create([{
+		title: "GMC Denali Road Bike",
+		price: 389,
+		body: "Mens GMC Denali Light weight aluminum road bike has a 21 speed Shimano derailleur"
+	},
+	{
+		title: "Ostrich Pillow",
+		price: 97,
+		body: "Ostrich Pillow is a revolutionary new product to enable easy power naps anytime, everywhere, Ostrich Pillow's unique design offers a micro environment in which to take a cozy and comfortable power nap at ease. Ostrich Pillow has been designed to allow you to create a little private space within a public one, to relax and unwind."
+	}])
+
+p "Created #{Post.count} posts"
