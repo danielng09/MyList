28200a4bf7a217904b3e36d089172f073e23b83b
Daniel Ng <dvng13@gmail.com>
2015-04-13 15:58:35 -0600

    Validates that amazon_id exists after creation of a post

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+	def setup
+		@post = Post.new(title: "Example", price: 99, body: "This is an example text", amazon_link: "http://www.amazon.com/gp/product/B00BGIQS1A")
+	end
+
+	# test "title should be present" do
+	# end
+
 	test "posts come out ordered by most recent" do
 		assert_equal Post.first, posts(:most_recent)
+	end
+
+	test "amazon_id is generated from new post" do
+		@post.save
+		assert_equal @post.amazon_id.exists?, "B00BGIQS1A"
 	end
 end
